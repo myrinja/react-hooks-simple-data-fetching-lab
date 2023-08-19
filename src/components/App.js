dcf3b4bd6ba684ebf1a07a7140c6d048a445b6bb
myrinja <myrinjau98@gmail.com>
2023-08-19 17:18:51 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,29 @@
 // create your App component here
+import react,{useState,useEffect} from "react"
+import { Response } from "whatwg-fetch"
+
+ function App(){
+    const[image, setImage] = useState(null)
+    
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(Response => Response.json())
+        .then(data => 
+            setImage(data.message))
+       
+    })
+    return(
+        <>
+ <div>
+      {image ? (
+        <img src={image} alt="A Random Dog" />
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+</>
+    )
+
+}
+ export default App;
+ 
